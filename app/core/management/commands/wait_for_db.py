51c73b6ae9ecbaf30ea3945ff7989597892ee1cd
bibhuprasad <bibhuprasadp530@gmail.com>
2023-01-02 23:42:57 +0530

    updated changes

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+class for app to esit for db to start
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self,*args,**options):
+        pass
