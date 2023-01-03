fc5bf31d2795fac190c7e7ece870020161934e4a
bibhuprasad <bibhuprasadp530@gmail.com>
2023-01-03 19:52:25 +0530

    chhanges for Linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,11 +14,11 @@ class Command(BaseCommand):
         """Entry point for command"""
         self.stdout.write("Waiting for database")
 
-        db_up= False
+        db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                db_up= True
+                db_up = True
             except(Psycopg2OpError, OperationalError):
                 self.stdout.write("database Unavialbale ...waiting for 1 sec")
                 time.sleep(1)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,7 +10,7 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
-        patched_check.return_value=True
+        patched_check.return_value = True
 
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
@@ -19,7 +19,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting when for database when operational error"""
 
-        patched_check.side_effect=[Psycopg2OpError]*2 + \
+        patched_check.side_effect = [Psycopg2OpError]*2 + \
             [OperationalError]*3 + [True]
 
         call_command('wait_for_db')
